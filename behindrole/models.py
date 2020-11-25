80b782b4961e372c9ad98bd57fa55aacfa475306
ALi <samadi_as@yahoo.com>
2020-11-25 16:46:29 +0330

    tweet body

--- behindrole/models.py
+++ behindrole/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class Tweet (models.Model):
     tweetTitle = models.TextField()
+    #author = models.ForeignKey('auth.user', on_delete=models.CASCADE,)
+    body = models.TextField()
 
     def __str__(self):
         return self.tweetTitle
