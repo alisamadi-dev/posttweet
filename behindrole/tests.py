109b4552d180cfe6bb0e228bfc8cb37a79766b4c
ALi <samadi_as@yahoo.com>
2020-11-25 19:58:59 +0330

    test

--- behindrole/tests.py
+++ behindrole/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.http import response
 from behindrole.models import Tweet
+from django.contrib.auth import get_user_model
 # Create your tests here.
 
 
@@ -15,20 +16,20 @@ from behindrole.models import Tweet
 #         response = self.client.get('/about/')
 #         self.assertEqual(response.status_code, 200)
 
-class TweetModelTests(TestCase):
+# class TweetModelTests(TestCase):
 
-    # setup a test database and create an object  fill the proper field of model.
-    def setUp(self):
-        Tweet.objects.create(tweetTitle="this is a tweet")
+#     # setup a test database and create an object  fill the proper field of model.
+#     def setUp(self):
+#         Tweet.objects.create(tweetTitle="this is a tweet")
 
-    # test content
+#     # test content
 
-    def test_text_content(self):
-        tweet = Tweet.objects.get(id=1)
-        # get value
-        expected_object_name = tweet.tweetTitle
-        # compare first record  from db and what is in object field from model
-        self.assertEqual(expected_object_name, "this is a tweet")
+#     def test_text_content(self):
+#         tweet = Tweet.objects.get(id=1)
+#         # get value
+#         expected_object_name = tweet.tweetTitle
+#         # compare first record  from db and what is in object field from model
+#         self.assertEqual(expected_object_name, "this is a tweet")
 
 
 class HomePageViewTest(TestCase):
@@ -47,3 +48,33 @@ class HomePageViewTest(TestCase):
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'home.html')
+
+
+class BlogTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret',
+        )
+        self.tweet = Tweet.objects.create(
+
+            tweetTitle='A good title',
+            body='Nice body content',
+
+
+        )
+
+    def test_string_presentation(self):
+        tweet = Tweet(tweetTitle="A sample title")
+        self.assertEqual(str(tweet), tweet.tweetTitle)
+
+    def test_tweet_content(self):
+        self.assertEqual(f"{self.tweet.tweetTitle}", 'A good title')
+        self.assertEqual(f"{self.tweet.body}", 'Nice body content')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
